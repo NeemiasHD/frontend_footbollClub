b3b8d1112ea8489a6beb92758264ff3ecb37ddef
Neemias Claudio <neemiasclaudio5@gmail.com>
2024-08-27 15:43:47 -0300

    teste 3

---
+++ app/Components/bannerPartida/BannerPrePartida.tsx
@@ -0,0 +1,197 @@
+"use client";
+import { RenderLogo } from "@/app/Context/BagresContext";
+import React, { useEffect, useState } from "react";
+
+interface PartidaProps {
+  tipoConfronto: string;
+  data: string;
+  nomeT1: string;
+  nomeT2: string;
+  logoT1: string;
+  logoT2: string;
+  Horario: string;
+  local: string;
+  id: number;
+}
+const BannerPrePartida: React.FC<PartidaProps> = ({
+  logoT1,
+  logoT2,
+  nomeT1,
+  nomeT2,
+  tipoConfronto,
+  data,
+  id,
+  Horario,
+  local,
+}) => {
+  const [imgt1, setImgt1] = useState<string>("");
+  const [imgt2, setImgt2] = useState<string>("");
+  const [imgt3, setImgt3] = useState<string>("");
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const logo = await RenderLogo(logoT1);
+      const logo2 = await RenderLogo(logoT2);
+      const logo3 = await RenderLogo(
+        "https://res.cloudinary.com/dtpsqmz73/image/upload/v1724644921/vkjxb2cwyq5rudnx4xuu.png"
+      );
+      if (logo) {
+        setImgt1(logo);
+      }
+      if (logo2) {
+        setImgt2(logo2);
+      }
+      if (logo3) {
+        setImgt3(logo3);
+      }
+    };
+    fetchImage();
+  }, [logoT1]);
+
+  return (
+    <div
+      id={`BannerPrePartida${id}`}
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "var(--corazul)",
+        height: "2560px",
+      }}
+    >
+      <div
+        style={{
+          position: "relative",
+          overflow: "hidden",
+        }}
+      >
+        <img
+          src={imgt3}
+          style={{
+            height: "2560px",
+            zIndex: "21",
+            position: "relative",
+            top: "1px",
+          }}
+        />
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          zIndex: 21,
+          top: 900,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "300px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <p
+            style={{
+              fontSize: "50px",
+              fontWeight: "800",
+              color: "white",
+              width: "400px",
+              textAlign: "center",
+              textShadow: "5px 5px 1px var(--cinza)",
+              height: "100px",
+            }}
+          >
+            {nomeT1.toLocaleUpperCase()}
+          </p>
+          <div
+            style={{
+              height: "500px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "end",
+            }}
+          >
+            <img src={imgt1} width={450} />
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <p
+            style={{
+              fontSize: "50px",
+              fontWeight: "800",
+              color: "white",
+              width: "400px",
+              textAlign: "center",
+              textShadow: "5px 5px 1px var(--cinza)",
+              height: "100px",
+            }}
+          >
+            {nomeT2.toLocaleUpperCase()}
+          </p>
+          <div
+            style={{
+              height: "500px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "end",
+            }}
+          >
+            <img src={imgt2} width={450} />
+          </div>
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            top: "700px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <p
+            style={{
+              color: "white",
+              fontSize: "50px",
+              textShadow: "0px 4px 5px var(--cinza)",
+              marginBottom: "60px",
+            }}
+          >
+            {tipoConfronto}
+          </p>
+          <p
+            style={{
+              color: "white",
+              fontSize: "80px",
+              fontWeight: "900",
+              textShadow: "0px 4px 5px var(--cinza)",
+            }}
+          >
+            {data} as {Horario}
+          </p>
+          <p
+            style={{
+              color: "white",
+              fontSize: "50px",
+              width: "700px",
+              textAlign: "center",
+              textShadow: "0px 4px 5px var(--cinza)",
+            }}
+          >
+            {local}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BannerPrePartida;
---
+++ app/Components/fotosSection/AdmSalvarFotos.tsx
@@ -0,0 +1,95 @@
+"use client";
+import React, { useState } from "react";
+import {
+  UploadImagemToClound,
+  UseBagresContext,
+} from "@/app/Context/BagresContext";
+import { GridLoader } from "react-spinners";
+import InputFoto from "./InputFoto";
+
+function AdmSalvarFotos() {
+  const [data, setData] = useState<string | null>(null);
+
+  const [descricao, setDescricao] = useState<string | null>(null);
+  const { setAtualizarNoticias, atualizarNoticias, ImagemUpload } =
+    UseBagresContext();
+  const [IsLoading, setIsLoading] = useState(false);
+
+  const handleCreateNoticias = async () => {
+    setIsLoading(true);
+    let imagem;
+    if (ImagemUpload) imagem = await UploadImagemToClound(ImagemUpload);
+    const foto = {
+      descricao: descricao,
+      fotoUrl: imagem,
+      data: data,
+      // Adicione outros campos aqui conforme necessário
+    };
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_BAGRES}foto`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(foto),
+        }
+      );
+
+      if (response.ok) {
+        // Notícia criada com sucesso
+        console.log(response);
+        setAtualizarNoticias(atualizarNoticias + 1);
+      } else {
+        // Erro ao criar notícia
+        alert("Erro ao criar foto");
+      }
+    } catch (error) {
+      console.error("Erro na requisição:", error);
+    }
+    setIsLoading(false);
+  };
+  return (
+    <div className="Adm">
+      <div
+        style={{
+          width: 350,
+          height: 196,
+          border: "3px dotted var(--cinza)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {IsLoading ? (
+          <GridLoader color="#00d2ff" />
+        ) : (
+          <InputFoto id="Fotosimput" />
+        )}
+      </div>
+      <input
+        type="date"
+        className="InputNoticia"
+        id="inputMensagem"
+        onChange={(e) => setData(e.target.value)}
+      />
+      <input
+        type="text"
+        className="InputNoticia"
+        id="inputMensagem"
+        placeholder="Descrição"
+        onChange={(e) => setDescricao(e.target.value)}
+      />
+      <div className="BtnEdit">
+        <p className="Btn Salvar" onClick={handleCreateNoticias}>
+          Salvar
+        </p>
+        <p className="Btn Limpar">Limpar</p>
+      </div>
+    </div>
+  );
+}
+
+export default AdmSalvarFotos;
